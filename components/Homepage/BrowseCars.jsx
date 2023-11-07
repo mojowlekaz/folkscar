cbf09830837cce9252d675fa482f2179eb0ea8ac
mojowlekaz <93149541+mojowlekaz@users.noreply.github.com>
2023-11-07 23:56:13 +0100

    update

--- components/Homepage/BrowseCars.jsx
+++ components/Homepage/BrowseCars.jsx
@@ -32,7 +32,7 @@ export default function BrowseCars() {
           </div>
         </div>
       </div>
-      <div className="flex flex-row   gap-5  flex-wrap">
+      <div class="flex flex-wrap gap-4 justify-center items-center">
         {cars.map((car) => (
           <CarUniversalCard key={car.id} vehicle={car} />
         ))}
