01911090d46385731c079210172397f792fb9597
mojowlekaz <93149541+mojowlekaz@users.noreply.github.com>
2023-11-18 18:36:18 +0100

    NFT Minting homepage

--- app/Nft/page.jsx
+++ app/Nft/page.jsx
@@ -2,12 +2,11 @@ import Home from "@/components/NFT/Home";
 import Navbar from "@/components/Minting/Navbar";
 import React from "react";
 import Perks from "@/components/NFT/Perks";
-import Works from "@/components/NFT/Works";
-import Steppershow from "@/components/NFT/Steppershow";
 import VerticalStepper from "@/components/NFT/VerticalStepper";
 import Faqs from "@/components/NFT/Faqs";
 import Footers from "@/components/NFT/Footers";
 import Socials from "@/components/NFT/Socials";
+import Works from "@/components/NFT/works";
 
 export default function page() {
   return (
