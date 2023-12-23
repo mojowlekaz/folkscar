0c2d0d5df178ab430a5634ee41141e61264f4899
mojowlekaz <93149541+mojowlekaz@users.noreply.github.com>
2023-12-24 00:54:23 +0100

    image path change

--- app/collections/page.jsx
+++ app/collections/page.jsx
@@ -19,7 +19,7 @@ export default function page() {
         width={100}
         height={100}
         layout="responsive"
-        src="https://s3-alpha-sig.figma.com/img/9c3a/0ead/12a9559ed7b2ba2cd623eb0fb5579478?Expires=1700438400&Signature=n1hk5cPJc0WrNYtGSS974Ecaka5rQBsTySowFVDyD9MiA86OzAhG9WsH3y4~5ZLJDSB4J1NTgGZadnh9CW71kuLa~6u8Xz3F5qCfUSrL4TkUbqxbV8GzJ9s03kk7hlKiEx~3hSB0nYFP1zS3KsBsXxMzo0cFBksDzBBXpbURj520LENfxgNDFTmcYu7JHrkUeQ9S7t3LxrMow0on7~cdcK5MKAYsfSfFlqRWLXoCpjALR-3vOwDIGWHIbMMEm~DUAf6tO71GPoMQcNDz-BBYbTWBpZMqnioL-0ODYfPn4kB34SmUDLxkQD5B1HR5k6DhFPHpE6eDBy~Uf1UWDJcgHA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        src="https://s3-alpha-sig.figma.com/img/9c3a/0ead/12a9559ed7b2ba2cd623eb0fb5579478?Expires=1704067200&Signature=Bllcll7J1yiMNcNmd8~x0wC6KjdRXjx2M5osjirx5ZNM7f19t2neAzhBkIwN51X9N78WjIBB1l4fhcGTw205UDGwSHolFGr3lcsYS0GEoqMSwkqHTUAN81-NQkJYN-N3t0HXoNb3lXkRArv1YTujT3GIxKYasCx-5cDZeyGGfOpJuTzvjJTJmEqmS4KUetQd6-Z1mdzqE2hKvfvcvMU~R9fytewtM3ZoPyRTCTHmQVlca5Zt--F9RU1~T77vN-aC7vmUQEq4EIs9tkaXhiHB~A-DbzayihIfH9K7pcYWNIn6V1p~ZKjfC-avbUTVNAoYEp0PvT5JpsUHutpiwc8~NA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
         alt="greencar"
       />
       <BrowseCars />
